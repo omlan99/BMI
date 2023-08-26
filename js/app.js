c4b993b3f7afdd62e1fee9ea8fdddc7cca96770a
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-08-27 01:22:55 +0600

    up

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ document.getElementById('btn').addEventListener('click',function(){
  let weight=document.getElementById('weight').value;
  let height=document.getElementById('height').value;
  let bmi=weight/(height*height);
- document.getElementById('result').innerHTML=bmi;
+ document.getElementById('result').innerHTML=`your current BMI is ${bmi}`;
 })
