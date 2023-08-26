4080dad9409c2024da68c3cbbb982bed3d720fb2
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-08-27 01:10:23 +0600

    java

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click',function(){
+ let weight=document.getElementById('weight').value;
+ let height=document.getElementById('height').value;
+ let bmi=weight/(height*height);
+ document.getElementById('result').innerHTML=bmi;
+})
